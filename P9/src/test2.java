fb90fb55a098c815ad3130a676005657dcc5fd2c
Arthy <Arthy@DESKTOP-L4HUAOJ>
2020-08-23 09:35:55 -0700

    Hi all

---
+++ P9/src/test2.java
@@ -0,0 +1,9 @@
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to Java Selenium Love working hard and making mistakes...One line added");
+	}
+
+}
